dff09e1e930d18d27eed112c8190268d0127a9c5
jakey766@163.com <zxc5811095>
2016-09-29 13:39:53 +0800

    menu auth

---
+++ src/main/java/com/pk/framework/service/BaseService.java
@@ -0,0 +1,40 @@
+package com.pk.framework.service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Service基类
+ * @author jiangkunpeng
+ *
+ */
+public class BaseService {
+	
+	private static final Map<String, Object> Cache = new ConcurrentHashMap<String, Object>();
+	
+	/**
+	 * 存缓存
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public boolean putIntoCache(String key, Object value){
+		Cache.put(key, value);
+		//以后可加远程缓存
+		return true;
+	}
+
+	/**
+	 * 取缓存
+	 * @param key
+	 * @return
+	 */
+	public Object getFromCache(String key){
+		Object obj = Cache.get(key);
+		if(obj==null){
+			//从远程缓存中取
+		}
+		return obj;
+	}
+	
+}
